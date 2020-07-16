9c45fa0beb09bf369c342643187d059a78057889
Scott Tolinski <scott.tolinski@gmail.com>
2020-07-16 11:35:13 -0600

    initial commit

---
+++ package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  name: 'apollo',
+  version: '4.1.0',
+  summary: 'Basic Apollo integration for Meteor apps',
+  git: 'https://github.com/apollographql/meteor-integration',
+})
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.9')
+  api.use(['ecmascript', 'accounts-base', 'check'])
+
+  api.mainModule('src/server.js', 'server')
+  api.mainModule('src/client.js', 'client', { lazy: true })
+})
+
+Npm.depends({
+  '@apollo/client': '3.0.0',
+})
+
+Package.onTest(function (api) {
+  api.use([
+    'ecmascript',
+    'meteortesting:mocha',
+    'practicalmeteor:chai',
+    'accounts-base',
+    'apollo',
+  ])
+
+  api.mainModule('tests/server.js', 'server')
+})
